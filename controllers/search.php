3598f104d1b3d204f83eb1b7055d55e6839f2353
Nick Maloney <ngmaloney@gmail.com>
2013-04-10 14:44:48 -0400

    Refactor search so it is self contained in one class.

--- controllers/search.php
+++ controllers/search.php
@@ -1,37 +1,12 @@
 <?php
 
 require_once 'search/search_api.php';
-require_once 'search/date_search_api.php';
 
 /*
 Controller name: Search
 Controller description: PODS compatible search controller
  */
 class JSON_API_Search_Controller {
-
-  /**
-    * Performs a daterange search on a given content type
-    * TODO: Add category/tag param?
-    * TODO: If date field isn't specified search by created on
-    * Params:
-    * from_date - Date, date format YYYY-MM-DD
-    * to_date - Date, date format YYYY-MM-DD
-    * date_field - String, custom field to search by.
-    * post_type - String, type of post to search by
-  */
-  public function date_search() {
-    global $json_api;
-    $args = array(
-      'from_date' => $json_api->query->from_date,
-      'to_date' => $json_api->query->to_date,
-      'date_field' => $json_api->query->date_field,
-      'post_type' => $json_api->query->post_type,
-    );
-    $search = new DateSearchAPI($args);
-    return array(
-     'posts' => $search->execute(),
-    );
-  }
 
   /**
    * Performs a faceted search on several parameters.
@@ -51,6 +26,11 @@ class JSON_API_Search_Controller {
      'post_type' => $json_api->query->post_type,
      'tags' => $json_api->query->tags,
      'tag_id' => $json_api->query->tag_id,
+     'date_field' => $json_api->query->date_field,
+     'from_date' => $json_api->query->from_date,
+     'to_date' => $json_api->query->to_date,
+     'order' => $json_api->query->order,
+     'orderby' => $json_api->query->orderby,
     );
     $search = new SearchAPI($args);
     return array(
--- controllers/search/search_api.php
+++ controllers/search/search_api.php
@@ -1,11 +1,21 @@
 <?php
 
 class SearchAPI {
+  var $from_date;
+  var $to_date;
+  var $date_field;
+  var $post_type;
   var $category_id;
   var $category_slug;
   var $tags;
   var $tag_id;
-  var $post_type;
+  var $order = 'DESC';
+  var $orderby = 'post_date';
+
+  const BETWEEN_QUERY = 'date_meta_between_query';
+  const FROM_QUERY = 'date_meta_from_query';
+  const TO_QUERY =  'date_meta_to_query';
+  const DEFAULT_QUERY = 'default_query';
 
   function __construct($params) {
     foreach($params as $key => $val) {
@@ -17,26 +27,93 @@ class SearchAPI {
 
   function execute() {
     global $json_api;
-    return $json_api->introspector->get_posts($this->query_params());
+    $query = [];
+    $query_type = $this->query_type();
+    if($query_type) {
+      $query_args = call_user_func(array(get_class($this), $query_type));
+      $query = $json_api->introspector->get_posts($query_args);
+    }
+    return $query;
   }
 
-  private function query_params() {
+  private function query_type() {
+    if(!empty($this->from_date) && !empty($this->to_date) && !empty($this->date_field)) {
+      return self::BETWEEN_QUERY;
+    }
+    elseif(!empty($this->from_date) && !empty($this->date_field)) {
+      return self::FROM_QUERY;
+    }
+    elseif(!empty($this->to_date) && !empty($this->date_field)) {
+      return self::TO_QUERY;
+    }
+    else {
+      return self::DEFAULT_QUERY;
+    }
+  }
+
+  private function meta_params($values, $operator) {
+    $query = array(
+      'meta_key' => $this->date_field,
+      'meta_query' => array(
+        array(
+          'key' => $this->date_field,
+          'value' => $values,
+          'compare' => $operator,
+          'type' => 'DATE'
+        ),
+      ),
+    );
+    return $query;
+  }
+
+  private function default_params() {
     $query = array();
-    if(!empty($this->category_id)) {
-      $query['cat'] = $this->category_id;
+
+    if(!empty($this->date_field)) {
+      $query['orderby'] = $this->date_field;
     }
-    if(!empty($this->category_slug)) {
-      $query['category_name'] = $this->category_slug;
+    else {
+      $query['orderby'] = $this->orderby;
     }
-    if(!empty($this->tags)) {
-      $query['tag_slug__in'] = $this->tags;
-    }
-    if(!empty($this->tag_id)) {
-      $query['tag_id'] = $this->tag_id;
-    }
+
+    $query['order'] = $this->order;
+
     if(!empty($this->post_type)) {
       $query['post_type'] = $this->post_type;
     }
+
+    if(!empty($this->category_id)) {
+      $query['cat'] = $this->category_id;
+    }
+
+    if(!empty($this->category_slug)) {
+      $query['category_name'] = $this->category_slug;
+    }
+
+    if(!empty($this->tags)) {
+      $query['tag_slug__in'] = $this->tags;
+    }
+
+    if(!empty($this->tag_id)) {
+      $query['tag_id'] = $this->tag_id;
+    }
+
     return $query;
+  }
+
+  private function date_meta_from_query() {
+    return $this->default_params() + $this->meta_params($this->from_date, '>=');
+  }
+
+  private function date_meta_to_query() {
+    return $this->default_params() +  $this->meta_params($this->to_date, '<=');
+  }
+
+  private function date_meta_between_query() {
+    return $this->default_params() + $this->meta_params(array($this->from_date, $this->to_date), 'BETWEEN');
+  }
+
+  private function default_query() {
+    return $this->default_params();
   }
 }
